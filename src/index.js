e27b13d0d1c60dcbad4e7b5f25859596dc01ba54
Étienne Després <etienne@pochour.com>
2018-08-06 18:44:53 -0400

    Update to Phaser 3.12.0-beta1
    We need this to fix a bug with setCrop()

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import 'phaser';
-import reset from 'reset-css';
+import 'reset-css';
 import '../assets/scss/styles.scss';
 import { Main } from './scenes';
+
+const width = 1920, height = 1080;
 
 var game = new Phaser.Game({
   type: Phaser.AUTO,
   parent: 'container',
-  width: 1920,
-  height: 1080,
+  width: width,
+  height: height,
   scene: Main,
   backgroundColor: 0x1b1862,
 });
+
+function resize() {
+  const ratio = width / height;
+  let canvas_height = window.innerHeight;
+  let canvas_width = canvas_height * ratio;
+
+  if (canvas_width > window.innerWidth) {
+    canvas_width = window.innerWidth;
+    canvas_height = canvas_width/ratio;
+  }
+
+  game.canvas.style.width = canvas_width + 'px';
+  game.canvas.style.height = canvas_height + 'px';
+}
+
+window.addEventListener('load', resize, false);
+window.addEventListener('resize', resize, false);
